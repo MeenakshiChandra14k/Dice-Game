310fe028b19c45a81346e77b57dfc106d83551fa
Meenakshi Chandra <146656763+MeenakshiChandra14k@users.noreply.github.com>
2024-06-27 21:13:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,26 +2,22 @@
 var randomNumber1;
 var randomNumber2;
 
-function changeImage(x){
-    if(x===1){
-        randomNumber1 = Math.round(Math.random()*5)+1;
-        var location="./images/dice"+randomNumber1+".png";
+function changeImage(){
+    randomNumber1 = Math.round(Math.random()*5)+1;
+    var location="./images/dice"+randomNumber1+".png";
+    document.getElementsByClassName("img1")[0].src=location;
+    randomNumber2 = Math.round(Math.random()*5)+1;
+    location="./images/dice"+randomNumber2+".png";
+    document.getElementsByClassName("img2")[0].src=location;
+    
+    if(randomNumber1>randomNumber2){
+        document.getElementsByTagName("h1")[0].innerHTML="🚩Player 1 Wins!";
+    }
+    else if(randomNumber1<randomNumber2){
+        document.getElementsByTagName("h1")[0].textContent="Player 2 Wins!🚩";
     }
     else{
-        randomNumber2 = Math.round(Math.random()*5)+1;
-        var location="./images/dice"+randomNumber2+".png";
+        document.getElementsByTagName("h1")[0].textContent="Draw!";
     }
-    image="img"+x;
-    document.getElementsByClassName(image)[0].src=location;
-    if(x===2){
-        if(randomNumber1>randomNumber2){
-            document.getElementsByTagName("h1")[0].innerHTML="🚩Player 1 Wins!";
-        }
-        else if(randomNumber1<randomNumber2){
-            document.getElementsByTagName("h1")[0].textContent="Player 2 Wins!🚩";
-        }
-        else{
-            document.getElementsByTagName("h1")[0].textContent="Draw!";
-        }
-    }
+    
 }
